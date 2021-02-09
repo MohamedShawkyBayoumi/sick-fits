b12ed26465902a74ef7cd0dc4810d693d51fb25d
Mohamed Shawky <mohamedshawkybayoumi@gmail.com>
2021-02-09 08:49:25 +0200

    feat: resolve css className comparison with server and client issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import Page from "../components/Page";
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+import 'nprogress/nprogress.css';
 
 export default function App({ Component, pageProps }){
     return (
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,16 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+
+    // to resolve css className comparison with server and client issue 
+    const sheet = new ServerStyleSheet();
+    const page = ctx.renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+
+    return { ...initialProps, ...page, styleTags }
   }
 
   render() {
